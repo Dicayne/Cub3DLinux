7928aa4e6a0bd886bfd5793bbd41ed09371de4b1
Victor Moreau <v.moreau96@hotmail.fr>
2020-06-13 02:35:14 +0200

    m_map print in img raycast

--- src/bonus/game/display/put_mini_map.c
+++ src/bonus/game/display/put_mini_map.c
@@ -6,7 +6,7 @@
 /*   By: vmoreau <vmoreau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 17:30:22 by vmoreau           #+#    #+#             */
-/*   Updated: 2020/04/08 16:45:15 by vmoreau          ###   ########.fr       */
+/*   Updated: 2020/06/13 02:33:58 by vmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,43 @@ static void		print_pos(t_cub3d *cub, t_image *m_map)
 
 	x = ((int)(cub->map.pos_x * 100) * m_map->img_w) / (cub->map.x * 100);
 	y = ((int)(cub->map.pos_y * 100) * m_map->img_h) / (cub->map.y * 100);
-	my_mlx_pixel_put(m_map, x, y, 0xffffff);
-	my_mlx_pixel_put(m_map, x + 1, y, 0xffffff);
-	my_mlx_pixel_put(m_map, x, y + 1, 0xffffff);
-	my_mlx_pixel_put(m_map, x - 1, y, 0xffffff);
-	my_mlx_pixel_put(m_map, x, y - 1, 0xffffff);
+	if (cub->boolimg == 1)
+	{
+		my_mlx_pixel_put(&cub->img, x, y, 0xffffff);
+		my_mlx_pixel_put(&cub->img, x + 1, y, 0xffffff);
+		my_mlx_pixel_put(&cub->img, x, y + 1, 0xffffff);
+		my_mlx_pixel_put(&cub->img, x - 1, y, 0xffffff);
+		my_mlx_pixel_put(&cub->img, x, y - 1, 0xffffff);
+	}
+	if (cub->boolimg == 2)
+	{
+		my_mlx_pixel_put(&cub->img2, x, y, 0xffffff);
+		my_mlx_pixel_put(&cub->img2, x + 1, y, 0xffffff);
+		my_mlx_pixel_put(&cub->img2, x, y + 1, 0xffffff);
+		my_mlx_pixel_put(&cub->img2, x - 1, y, 0xffffff);
+		my_mlx_pixel_put(&cub->img2, x, y - 1, 0xffffff);
+	}
 }
 
-static void		back_map(t_image *m_map)
+static void		back_map(t_image *m_map, t_cub3d *cub)
 {
 	int x;
 	int y;
+	int color;
 
+	color = 0x000000;
 	y = 0;
 	while (y < m_map->img_h)
 	{
 		x = 0;
 		while (x < m_map->img_w)
 		{
-			my_mlx_pixel_put(m_map, x, y, 0xa6000000);
+			if (cub->boolimg == 1)
+				my_mlx_pixel_put(&cub->img, m_map->img_w + x,
+									m_map->img_h + y, color);
+			if (cub->boolimg == 2)
+				my_mlx_pixel_put(&cub->img2, m_map->img_w + x,
+									m_map->img_h + y, color);
 			x++;
 		}
 		y++;
@@ -57,8 +75,9 @@ static void		base_m_map(t_cub3d *cub, t_image *m_map)
 {
 	int x;
 	int y;
+	int color;
 
-	back_map(m_map);
+	back_map(m_map, cub);
 	y = 0;
 	while (y < m_map->img_h)
 	{
@@ -67,10 +86,16 @@ static void		base_m_map(t_cub3d *cub, t_image *m_map)
 		{
 			if (cub->map.map[(y * cub->map.y) / m_map->img_h]
 							[(x * cub->map.x) / m_map->img_w] == 1)
-				my_mlx_pixel_put(m_map, x, y, 0xff0000);
+				color = 0xff0000;
 			if (cub->map.map[(y * cub->map.y) / m_map->img_h]
 							[(x * cub->map.x) / m_map->img_w] == 2)
-				my_mlx_pixel_put(m_map, x, y, 0xff);
+				color = 0xff;
+			if (cub->boolimg == 1)
+				my_mlx_pixel_put(&cub->img, m_map->img_w + x,
+									m_map->img_h + y, color);
+			if (cub->boolimg == 2)
+				my_mlx_pixel_put(&cub->img2, m_map->img_w + x,
+									m_map->img_h + y, color);
 			x++;
 		}
 		y++;
@@ -84,7 +109,5 @@ void			put_mini_map(t_cub3d *cub)
 
 	init_img_mmap(cub, &m_map);
 	base_m_map(cub, &m_map);
-	mlx_put_image_to_window(cub->map.mlx_ptr, cub->map.mlx_win,
-														m_map.img, 0, 0);
-	mlx_destroy_image(cub->map.mlx_ptr, m_map.img);		
+	mlx_destroy_image(cub->map.mlx_ptr, m_map.img);
 }
